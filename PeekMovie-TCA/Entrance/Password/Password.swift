33645af16e3443741010589a946b076840680467
Beka-l3 <b.talgat@innopolis.university>
2023-09-07 20:14:11 +0300

    Initial setip of password page

--- PeekMovie-TCA/Entrance/Password/Password.swift
+++ PeekMovie-TCA/Entrance/Password/Password.swift
@@ -9,8 +9,21 @@ import ComposableArchitecture
 
 struct Password: Reducer {
     
-    func reduce(into state: inout State, action: Action) -> Effect<Action> {
-        return .none
+    init() {
+        
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case let .view(.didChangePassword(newValue)):
+                state.password = newValue
+                return .none
+                
+            default:
+                return .none
+            }
+        }
     }
 }
 
@@ -18,7 +31,30 @@ struct Password: Reducer {
 // MARK: - Action
 extension Password {
     enum Action: Equatable, Sendable {
-        
+        case delegate(Delegate)
+        case view(View)
+        case _private(Private)
+    }
+}
+
+extension Password.Action {
+    enum Delegate: Equatable, Sendable {
+        case checkPassword
+        case forgotPassword
+    }
+}
+
+extension Password.Action {
+    enum View: Equatable, Sendable {
+        case didChangePassword(String)
+        case didTapOnContinue
+    }
+}
+
+extension Password.Action {
+    enum Private: Equatable, Sendable {
+        case passwordIsValid
+        case passwordIsNotValid
     }
 }
 
@@ -29,6 +65,20 @@ extension Password {
 
 extension Password {
     struct State: Equatable, Sendable {
+        var password: String
         
+        var isFetching: Bool
+        
+        var isPerformingPasswordCheck: Bool {
+            password.isEmpty || isFetching
+        }
+        
+        init(
+            password: String = .empty,
+            isFetching: Bool = false
+        ) {
+            self.password = password
+            self.isFetching = isFetching
+        }
     }
 }
--- PeekMovie-TCA/Entrance/Password/PasswordView.swift
+++ PeekMovie-TCA/Entrance/Password/PasswordView.swift
@@ -10,15 +10,70 @@ import ComposableArchitecture
 
 struct PasswordView: View {
     
+    private let store: StoreOf<Password>
+    
     init(store: StoreOf<Password>) {
-        
+        self.store = store
     }
     
     var body: some View {
-        ZStack {
-            Color(.green).ignoresSafeArea()
-            
-            Text("Password")
+        WithViewStore(store, observe: { $0 }) { viewStore in
+            ZStack {
+                Color(.orange)
+                    .ignoresSafeArea()
+                
+                VStack(spacing: 24) {
+                    HStack(spacing: 8) {
+                        Text("id")
+                            .foregroundColor(.white)
+                            .font(.title.bold())
+                        
+                        Assets.Icon.app(.x48)
+                            
+                    }
+                    .padding(.vertical, 120)
+                    
+                    TextField("Password", text: viewStore.binding(get: \.password, send: { .view(.didChangePassword($0)) } ) )
+                        .autocorrectionDisabled()
+                        .textContentType(.password)
+                        .frame(maxWidth: 240)
+                        .padding()
+                        .background {
+                            Color(.black)
+                                .opacity(0.1)
+                        }
+                        .mask {
+                            RoundedRectangle(cornerRadius: 16)
+                        }
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 16)
+                                .stroke(lineWidth: 2)
+                        }
+                    
+                    
+                    Button {
+                        viewStore.send(.view(.didTapOnContinue))
+                    } label: {
+                        ZStack {
+                            Text("Continue")
+                                .foregroundColor(.white.opacity(viewStore.isPerformingPasswordCheck ? 0.3 : 1))
+                                .frame(width: 280, height: 50)
+                                .background(.black.opacity(viewStore.isPerformingPasswordCheck ? 0.3 : 1))
+                                .font(.system(size: 20, weight: .bold))
+                                .cornerRadius(10)
+
+                            if viewStore.isFetching {
+                                ProgressView()
+                            }
+                        }
+
+                    }
+                    .disabled(viewStore.isPerformingPasswordCheck)
+                    
+                    
+                    Spacer()
+                }
+            }
         }
         
     }
