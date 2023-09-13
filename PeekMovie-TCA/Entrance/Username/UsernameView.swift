376d3f6ed5af51a88213302e45acf7b063a254c5
Beka-l3 <b.talgat@innopolis.university>
2023-09-13 16:16:38 +0300

    Remove comments

--- PeekMovie-TCA/Entrance/Username/UsernameView.swift
+++ PeekMovie-TCA/Entrance/Username/UsernameView.swift
@@ -45,43 +45,15 @@ struct UsernameView: View {
         NavigationStackStore(
             self.store.scope(state: \.path, action: Username.Action.path)
         ) {
-            
             WithViewStore(store, observe: { $0 }) { viewStore in
                 ZStack {
                     Color(.black)
                         .ignoresSafeArea()
                     
                     VStack(spacing: 24) {
-//                        Assets.Icon.app(.x128)
-//                            .padding(24)
-//                            .padding(.top, 120)
                         PeekLogo()
                         
                         UsernameTextField(text: viewStore.binding(get: \.username, send: {.view(.didChangeUsername($0))}  ))
-//                        TextField(String.empty, text: viewStore.binding(get: \.username, send: { .view(.didChangeUsername($0)) } ))
-//                            .autocorrectionDisabled()
-//                            .foregroundColor(.white)
-//                            .placeholder(when: viewStore.username.isEmpty) {
-//                                Text("Username")
-//                                    .foregroundColor(.gray)
-//                            }
-//                            .frame(maxWidth: .infinity)
-//                            .padding()
-//                            .background {
-//                                Color(.yellow)
-//                                    .opacity(0.1)
-//                            }
-//                            .mask {
-//                                RoundedRectangle(cornerRadius: 12)
-//                            }
-//                            .overlay {
-//                                RoundedRectangle(cornerRadius: 12)
-//                                    .stroke(lineWidth: 1)
-//                                    .foregroundColor(.yellow)
-//
-//                            }
-//                            .padding(.horizontal, 48)
-                        
                         
                         Button {
                             viewStore.send(.view(.didTapOnContinue))
@@ -90,24 +62,8 @@ struct UsernameView: View {
                                 isPerformingPasswordCheck: viewStore.binding(get: \.isPerformingUsernameCheck, send: .view(.getValue)),
                                 isFetching: viewStore.binding(get: \.isFetching, send: .view(.getValue))
                             )
-//                            ZStack {
-//                                Text("Continue")
-//                                    .foregroundColor(.black.opacity(viewStore.isPerformingUsernameCheck ? 0.3 : 1))
-////                                    .frame(width: 280, height: 50)
-//                                    .frame(maxWidth: .infinity, maxHeight: 50)
-//                                    .background(.yellow.opacity(viewStore.isPerformingUsernameCheck ? 0.3 : 1))
-//                                    .font(.system(size: 20, weight: .bold))
-//                                    .cornerRadius(12)
-//                                    .padding(.horizontal, 48)
-//
-//                                if viewStore.isFetching {
-//                                    ProgressView()
-//                                }
-//                            }
-                                
                         }
                         .disabled(viewStore.isPerformingUsernameCheck)
-                        
                         
                         Spacer()
                         
@@ -115,14 +71,6 @@ struct UsernameView: View {
                             print("\nRegister tapped")
                         } label: {
                             RegisterButton(isFetching: viewStore.binding(get: \.isFetching, send: .view(.getValue)))
-//                            Text("Register")
-//                                .foregroundColor(.white)
-////                                .frame(width: 280, height: 50)
-//                                .frame(maxWidth: .infinity, maxHeight: 50)
-//                                .background(.purple)
-//                                .font(.system(size: 20, weight: .bold))
-//                                .cornerRadius(10)
-//                                .padding(.horizontal, 48)
                         }
                         .padding(.bottom, 40)
                     }
